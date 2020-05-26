16eff75996ec52003e27ddb0b3b157266324568b
Eduardo Eli <55281597+elieduardo@users.noreply.github.com>
2020-05-26 18:44:10 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -9,43 +9,83 @@ char palavra[5][20];
 char resposta[5][20];
 int acertos = 0;
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/*https://pt.stackoverflow.com/questions/220055/d%C3%BAvida-sobre-struct-e-ponteiro-para-struct*/
 
+int contagemPalavras(){
+	char c;
+	FILE *fp;
+	int count = 0;
+	int flag = 0;
+
+	if ((fp=fopen("teste.txt", "r"))==NULL)
+	{
+		printf("DEU ERRO AO ABRIR O ARQUIVO");
+		exit(1);
+	}
+
+	while(!feof(fp))
+	{
+		c = getc(fp);
+		if ( c == ' ' || c == '\n' )
+		{
+			if (flag == 0)
+			{
+				count++;
+				flag = 1;
+			}
+		}
+		else
+		{
+			if ( flag == 1 )
+				flag = 0;
+		}
+	}
+
+	fclose(fp);
+	return count;
+}
 void sorteioPalavras(int i){
-		sleep(1); 
-		FILE* f;
+	sleep(1); 
+	FILE* f;
+
+	f = fopen("teste.txt", "r");
+	if(f == 0) {
+		printf("Banco de dados de palavras não disponível\n\n");
+		exit(1);
+	}
+
+	int qtddepalavras = contagemPalavras();
 	
-		f = fopen("teste.txt", "r");
-		if(f == 0) {
-			printf("Banco de dados de palavras não disponível\n\n");
-			exit(1);
-		}
+	srand(time(NULL));
+	int randomico = rand() % qtddepalavras;
+
+	for(int j = 0; j <= randomico; j++) {
+		fscanf(f, "%s", palavra[i]);
+	}
 	
-		int qtddepalavras;
-		fscanf(f, "%d", &qtddepalavras);
-	
-		srand(time(NULL));
-		int randomico = rand() % qtddepalavras;
-	
-		for(int j = 0; j <= randomico; j++) {
-			fscanf(f, "%s", palavra[i]);
-		}
-		
-		fclose(f);		
-		i++;
+	fclose(f);	
+}
+
+void cronometro ( int segundos)
+{
+  clock_t fimtempo;
+  fimtempo = clock () + segundos * CLK_TCK;
+  while (clock() < fimtempo) {}
 }
 
 void mostraPalavras(){
+	
 	int j=5;	
 	do{
 		system ("cls");
 		printf ("Decore as palavras abaixo em %d\n\n\n", j);
 		j--;
 		for (int i=0;i<5;i++){
-			printf ("%s\t", palavra[i]);
+			printf ("\t(%d)%s\n", i+1, palavra[i]);
 		}
-		Sleep (1000);
+		cronometro(1);	
 	}while (j>0);
+	
 }
 
 void pedePalavras(){
@@ -53,12 +93,13 @@ void pedePalavras(){
 	printf ("Informe as palavras que foram exibidos\n\n");
 	
 	for (int i = 0; i < 5; i++){
-		printf ("\t%° palavra: ", i+1);
+		printf ("\t%d° palavra: ", i+1);
 		scanf ("%s", resposta[i]);
 	}
 }
 
 void verificaPalavras(){
+	
 	for(int i = 0; i <= 4; i ++){
 		int retorno = strcmp(resposta[i], palavra[i]);				
 		if(retorno == 0){
@@ -68,61 +109,106 @@ void verificaPalavras(){
 }
 
 
-
 void cadastrarPalavras(){
-	char ask;
+	
+	char novapalavra[20];
 
-	printf("Você gostaria de cadastrar uma nova palavra? (S/N)?");
+	printf("Digite a nova palavra, em letras maiúsculas: ");
+	scanf("%s", novapalavra);
+
+	FILE* f;
+
+	f = fopen("teste.txt", "r+");
+	if(f == 0) {
+		printf("Banco de dados de palavras não disponível\n\n");
+		exit(1);
+	}
+
+	int qtd;
+	fscanf(f, "%d", &qtd);
+	qtd++;
+	fseek(f, 0, SEEK_SET);
+	fprintf(f, "%d", qtd);
+
+	fseek(f, 0, SEEK_END);
+	fprintf(f, "\n%s", novapalavra);
+
+	fclose(f);
+				
+	char ask = 'S';
+	
+	printf("Você gostaria de cadastrar outra palavra? (S/N)");
 	scanf(" %c", &ask);
-
-	if(ask == 'S') {
-		char novapalavra[20];
-
-		printf("Digite a nova palavra, em letras maiúsculas: ");
-		scanf("%s", novapalavra);
-
-		FILE* f;
-
-		f = fopen("teste.txt", "r+");
-		if(f == 0) {
-			printf("Banco de dados de palavras não disponível\n\n");
-			exit(1);
-		}
-
-		int qtd;
-		fscanf(f, "%d", &qtd);
-		qtd++;
-		fseek(f, 0, SEEK_SET);
-		fprintf(f, "%d", qtd);
-
-		fseek(f, 0, SEEK_END);
-		fprintf(f, "\n%s", novapalavra);
-
-		fclose(f);
-
+	
+	if(ask == 'S') {			
+		cadastrarPalavras();
+	}else{
+		exit(1);
 	}
 }
 
-int main(int argc, char *argv[]) {
-	setlocale(LC_ALL, "Portuguese");
-	int opcao;
-	system ("cls");
-	printf("----- Jogo da memória -----\n");
-	printf("(1) Jogar \n");
-	printf("(2) Como Jogar?\n");
-	printf("(3) Cadastrar Palavras\n");
-	printf("(0) Sair\n");
-	scanf("%d", &opcao);
-	
-	for(int i = 0; i <=4; i++){
-		sorteioPalavras(i);
-	}
-	mostraPalavras();
-	pedePalavras();
-	verificaPalavras();
-	
+void verificaGanhador(){
 	printf("%d \n", acertos);
+	system("pause");
+	//Fazer a verificação se acertos == 5
+	//Pergutar se deseja sair do jogo ou jogar novamente
+}
+
+int main(int argc, char *argv[]) {	
 	
-	cadastrarPalavras();
+	int opcao;
+	FILE *pont_regras;
+	char texto_str[200];
+	
+	
+	do{
+		acertos = 0;
+		setlocale(LC_ALL, "Portuguese");		
+		system ("cls");
+		printf("----- Jogo da memória -----\n");
+		printf("(1) Jogar \n");
+		printf("(2) Como Jogar?\n");
+		printf("(3) Cadastrar Palavras\n");
+		printf("(0) Sair\n");
+		scanf("%d", &opcao);
+		
+		switch(opcao){
+			case 1:
+				system ("cls");
+				printf("----- Sorteando as Palavras -----\n");
+				printf("------------ Aguarde -------------\n");
+				for(int i = 0; i <=4; i++){
+					sorteioPalavras(i);
+				}				
+				
+				mostraPalavras();
+				pedePalavras();
+				verificaPalavras();
+				verificaGanhador();
+				break;
+				
+			case 2:								
+				pont_regras = fopen("regras.txt", "r");
+				
+				while(fgets(texto_str, 200, pont_regras) != NULL)
+				printf("%s", texto_str);
+				
+				fclose(pont_regras);
+				system("pause");
+				
+				break;
+			
+			case 3:
+				cadastrarPalavras();
+				break;
+			case 0: 
+				opcao = 0;
+				break;
+			default:
+				printf("\nOpcao Invalida...\n");
+				break;
+		}
+	}while(opcao != 0);
+		
 	return 0;
 }
